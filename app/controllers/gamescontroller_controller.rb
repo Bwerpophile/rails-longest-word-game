7df3d58aa7f4d457cdeb539295de67fcaa5119c0
Joris Lespagnol <lespagnol.joris@gmail.com>
2019-03-11 17:21:43 +0100

    rails words game

---
+++ app/controllers/gamescontroller_controller.rb
@@ -0,0 +1,9 @@
+class GamescontrollerController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a.sample }
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'new', to: 'gamescontroller#new'
+  get 'score', to: 'gamescontroller#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
